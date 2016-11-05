a13671773368425162a32bd8e1d2792396b4d8a0
Carlos Alfaro <carlosalfarovargas@macbook-pro-de-carlos.local>
2016-11-05 08:38:21 -0600

    Initial Commit

---
+++ Quiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by Carlos Alfaro Vargas on 5/11/16.
+//  Copyright © 2016 Carlos Alfaro Vargas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Quiz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Quiz
+//
+//  Created by Carlos Alfaro Vargas on 5/11/16.
+//  Copyright © 2016 Carlos Alfaro Vargas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
